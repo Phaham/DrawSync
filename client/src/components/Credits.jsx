465e5d130ef107827ed78f5eed96e44ee2efc786
Phaham Khan <phaham17@gmail.com>
2025-06-10 22:37:25 +0530

    added backend files

---
+++ client/src/components/Credits.jsx
@@ -0,0 +1,11 @@
+import { Github } from "../assets/icons";
+
+export default function Credits() {
+  return (
+    <section className="credits">
+      <a href="https://phaham.netlify.app/" id="credits">
+        <Github /> Creator's Portfolio
+      </a>
+    </section>
+  );
+}
